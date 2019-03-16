9e9925a8633a5877626a70930bf6784140b22584
남동훈 <dhnam@wantedlab.com>
2019-03-16 21:06:24 +0900

    eslint추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
+export interface Props {
+  title: string;
+}
 
 export default class extends React.Component<Props, {}> {
   constructor(props) {
     super(props);
   }
   render() {
-    return <div>test</div>;
+    return <div>t est </div>;
   }
 }
