41e32961789f18d62194abdf5a88b6a89a9fda0b
jungdu <jungdujang@gmail.com>
2019-03-19 02:31:07 +0900

    autocomplete 라이브러리 추가, 예제 컴포넌트 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,6 +2,7 @@ import * as moment from 'moment';
 import dynamic from 'next/dynamic';
 import * as React from 'react';
 import DynamicComponentWithCustomLoading from '../common/calendar/DayPickerRange';
+import DownshiftEx from '../common/downshift/DownshiftEx';
 
 // const DynamicComponentWithCustomLoading = dynamic(() => import('../common/DayPickerRange'), {
 //   loading: () => <p>...</p>,
@@ -11,7 +12,12 @@ interface IndexProps {}
 
 class Index extends React.Component<IndexProps> {
   render() {
-    return <DynamicComponentWithCustomLoading />;
+    return (
+      <React.Fragment>
+        <DynamicComponentWithCustomLoading />
+        <DownshiftEx />
+      </React.Fragment>
+    );
   }
 }
 
